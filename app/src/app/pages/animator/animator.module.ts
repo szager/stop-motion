bf57be7a6f43f8066fe1330a6426ba5902e63d17
Lisa <ll@webpunks.co>
2021-07-27 09:22:40 +0200

    shifted all buttons from tab- and toolbar in corresponding components

--- app/src/app/pages/animator/animator.module.ts
+++ app/src/app/pages/animator/animator.module.ts
@@ -2,12 +2,28 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ComponentsModule } from '@components/components.module';
 import { AnimatorPage } from './animator.page';
-import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CaptureButtonComponent } from './components/capture-button/capture-button.component';
+import { ClearButtonComponent } from './components/clear-button/clear-button.component';
+import { LoadButtonComponent } from './components/load-button/load-button.component';
+import { PlayButtonComponent } from './components/play-button/play-button.component';
+import { RecordAudioButtonComponent } from './components/record-audio-button/record-audio-button.component';
+import { SaveButtonComponent } from './components/save-button/save-button.component';
 import { TabbarComponent } from './components/tabbar/tabbar.component';
+import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { UndoButtonComponent } from './components/undo-button/undo-button.component';
 
 const COMPONENTS = [
+  CaptureButtonComponent, 
+  ClearButtonComponent,
+  LoadButtonComponent,
+  PlayButtonComponent,
+  RecordAudioButtonComponent,
+  SaveButtonComponent,
+  TabbarComponent,
+  ToggleButtonComponent,  
   ToolbarComponent,
-  TabbarComponent 
+  UndoButtonComponent 
 ];
 
 @NgModule({
