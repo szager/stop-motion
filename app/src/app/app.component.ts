941a8ed11311bf7b8457a254a107d72bc0e56692
Christofer Huber <ch@webpunks.co>
2021-08-11 15:10:16 +0200

    Added servie worker update listener + alert

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BaseService } from '@services/base/base.service';
+import { ServiceWorkerService } from '@services/service-worker/service-worker.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
-    private baseService: BaseService
+    private baseService: BaseService,
+    private serviceWorkerService: ServiceWorkerService
   ) {
     this.baseService.translate.setDefaultLang('de');
+  }
+
+  async ngOnInit() {
+    // init service worker service
+    await this.serviceWorkerService.initListener();
   }
 }
