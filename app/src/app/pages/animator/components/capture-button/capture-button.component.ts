a8e943b0cc7b99af9d315bcbe50df500c15cad8a
Lisa <ll@webpunks.co>
2021-12-10 11:38:45 +0100

    Added base animation for capture button

--- app/src/app/pages/animator/components/capture-button/capture-button.component.ts
+++ app/src/app/pages/animator/components/capture-button/capture-button.component.ts
@@ -7,7 +7,7 @@ import { AnimatorService } from '@services/animator/animator.service';
   styleUrls: ['./capture-button.component.scss'],
 })
 export class CaptureButtonComponent implements OnInit {
-
+  animated: boolean = false;
   constructor(
     private animatorService: AnimatorService
   ) { }
@@ -16,6 +16,12 @@ export class CaptureButtonComponent implements OnInit {
 
   async onClick() {
     await this.animatorService.capture();
+    this.animated = !this.animated;
+    this.delay(500).then(() => this.animated = false);
+  }
+
+  async delay(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
 }
